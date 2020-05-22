01b4216ac09d43b834e7c19df967a53195c7951b
Philipp Hemmer <philipp.hemmer@chello.at>
2020-05-22 12:16:16 +0200

    blanks filter navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -17,7 +17,12 @@
                 .
                 </v-col>   
                 </v-row>
-                <v-row class="mt-18">
+                <v-row class="mt-18" v-for="song in songs" :key="song.id">
+                    <v-col>
+                        {{ song.title | blanked }}
+                    </v-col>   
+                </v-row>
+                <!--<v-row>
                 <v-col>
                 t o u c h
                 </v-col>   
@@ -26,14 +31,52 @@
                 <v-col>
                 t o u c h
                 </v-col>   
-                </v-row>
-                <v-row>
-                <v-col>
-                t o u c h
-                </v-col>   
-                </v-row>
+                </v-row>-->
                 
             </v-col>
         </v-layout>
      </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            songs: [
+                {
+                    id: 0,
+                    title: 'with the wolves',
+                    songtext: ''
+                },
+                {
+                    id: 1,
+                    title: 'touch',
+                    songtext: ''
+                },
+                {
+                    id: 2,
+                    title: 'touch2',
+                    songtext: ''
+                },
+            ]
+        }
+    },
+
+    filters: {
+        blanked(value) {
+            let sReturn = "";
+            for (var i = 0; i < value.length; i++) {
+             sReturn += value[i];
+             
+            if (!(i === value.length -1)) {
+                if (value[i] == " ") {
+                    sReturn += "\xa0";
+                }
+                sReturn += " ";
+             }
+            }
+            return sReturn;
+        }
+    }
+}
+</script>
